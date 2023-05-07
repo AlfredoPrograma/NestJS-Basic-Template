0e8c3222c7a74ef8a959d8d788df8eca153b047d
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-05-07 12:35:02 -0400

    feature: set up global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+function setUpConfigs(app: INestApplication) {
+  app.setGlobalPrefix('/api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      enableDebugMessages: process.env.NODE_ENV === 'development',
+    }),
+  );
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('/api/v1');
+  setUpConfigs(app);
   await app.listen(3000);
 }
+
 bootstrap();
