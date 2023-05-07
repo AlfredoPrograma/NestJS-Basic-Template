8578ae0ff639225a494f63d9420eefd493eebe3d
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-05-07 18:46:42 -0400

    feature: implement hashing service

--- src/core/services/encrypt.service.ts
+++ src/core/services/encrypt.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class EncryptService {}
+export class EncryptService {
+  private DIFFICULTY = 12;
+  private encrypter = bcrypt;
+
+  async hashPassword(password: string): Promise<string> {
+    return await this.encrypter.hash(password, this.DIFFICULTY);
+  }
+
+  async comparePassword(password: string, hash: string): Promise<boolean> {
+    return await this.encrypter.compare(password, hash);
+  }
+}
--- src/core/services/test/encrypt.service.spec.ts
+++ src/core/services/test/encrypt.service.spec.ts
@@ -15,4 +15,40 @@ describe('TestService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should encrypt password', async () => {
+    // arrange
+    const password = 's3cret-passw0rd';
+
+    // act
+    const hash = await service.hashPassword(password);
+
+    // assert
+    expect(hash).toBeDefined();
+    expect(hash).not.toEqual(password);
+  });
+
+  it('should return true in valid password', async () => {
+    // arrange
+    const password = 's3cret-passw0rd';
+    const hash = await service.hashPassword(password);
+
+    // act
+    const isValid = await service.comparePassword(password, hash);
+
+    // assert
+    expect(isValid).toBe(true);
+  });
+
+  it('should return false in invalid password', async () => {
+    // arrange
+    const password = 's3cret-passw0rd';
+    const hash = await service.hashPassword(password);
+
+    // act
+    const isValid = await service.comparePassword('invalid-password', hash);
+
+    // assert
+    expect(isValid).toBe(false);
+  });
 });
