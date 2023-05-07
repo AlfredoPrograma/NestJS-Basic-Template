2effb12a138ca14206f0102157af484d6d09dac1
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-05-07 13:50:47 -0400

    feature: add type orm initialization for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { TypeOrmConfigService } from './core/services/type-orm-config.service';
+
 import { ExampleModule } from './example/example.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ExampleModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+    ExampleModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [TypeOrmConfigService],
 })
 export class AppModule {}
--- src/env.d.ts
+++ src/env.d.ts
@@ -8,5 +8,6 @@ interface Environment {
   DB_PASSWORD: string;
   DB_NAME: string;
   DB_HOST: string;
+  DB_PORT: number;
   NODE_ENV: 'development' | 'production';
 }
