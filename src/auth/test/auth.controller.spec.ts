c5c458ed33fb8b3c5c3455bcb3805be419fbd494
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-05-10 00:08:18 -0400

    test: add sign up test for auth controller

--- src/auth/test/auth.controller.spec.ts
+++ src/auth/test/auth.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { EncryptService } from 'auth/encrypt.service';
+import { User } from 'core/models/user';
 import { UsersService } from 'users/users.service';
 
 import { AuthController } from '../auth.controller';
 import { AuthService } from '../auth.service';
 
 import { MockEncryptService, MockUsersService } from './mocks';
+import { createUserStub, newUserStub } from './stubs';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -32,5 +34,15 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a successful user sign up response', async () => {
+    const result = await controller.signUp(createUserStub);
+
+    const expected: { user: User } = {
+      user: newUserStub,
+    };
+
+    expect(result).toEqual(expected);
   });
 });
