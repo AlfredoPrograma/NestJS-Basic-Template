c4812365e6dba251a279d686aa744d431b35c1bb
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-05-14 23:51:02 -0400

    feature: initialize encrypt service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { EncryptService } from './encrypt.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, EncryptService],
 })
 export class AuthModule {}
