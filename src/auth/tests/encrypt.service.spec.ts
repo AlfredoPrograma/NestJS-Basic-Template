77bd235731022aec394509210864f16fdde6f5b6
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-05-15 00:34:04 -0400

    feature: implement encrypt and validate password methods

--- src/auth/tests/encrypt.service.spec.ts
+++ src/auth/tests/encrypt.service.spec.ts
@@ -15,4 +15,44 @@ describe('EncryptService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return encrypted password', async () => {
+    // Arrange
+    const password = '123456';
+
+    // Act
+    const expectedPassword = await service.encryptPassword(password);
+
+    // Assert
+    expect(expectedPassword).not.toBeNull();
+    expect(expectedPassword).not.toEqual(password);
+  });
+
+  it('should return true when password is valid', async () => {
+    // Arrange
+    const password = '123456';
+    const encryptedPassword = await service.encryptPassword(password);
+
+    // Act
+    const isValid = await service.validatePassword(password, encryptedPassword);
+
+    // Assert
+    expect(isValid).toBe(true);
+  });
+
+  it('should return false when password is invalid', async () => {
+    // Arrange
+    const password = '123456';
+    const invalidPassword = '1nvalid';
+    const encryptedPassword = await service.encryptPassword(password);
+
+    // Act
+    const isValid = await service.validatePassword(
+      invalidPassword,
+      encryptedPassword,
+    );
+
+    // Assert
+    expect(isValid).toBe(false);
+  });
 });
