040444e95dccc2532786b8021cf1c5a1168d0703
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-05-16 18:34:54 -0400

    feature: add sign up method in auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { EncryptService } from './encrypt.service';
+import { UsersService } from '@/users/users.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, EncryptService],
+  providers: [AuthService, EncryptService, UsersService],
 })
 export class AuthModule {}
