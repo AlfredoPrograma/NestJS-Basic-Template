3105de902e46e38b39244da44b1d131eda3c4e3e
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-05-17 00:10:45 -0400

    feature: add error handling for sign up controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,36 @@
-import { CreateUserDto } from '@/core/models/user';
-import { Body, Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  InternalServerErrorException,
+  Post,
+} from '@nestjs/common';
 
+import { ErrorHandlers } from '@/core/errors/types';
+import { CreateUserDto } from '@/core/models/user';
+
+import { AuthService } from './auth.service';
+import { UserErrors } from '@/users/errors/user.errors';
+
+type ControlledErrorKeys = UserErrors.USER_ALREADY_EXISTS;
 @Controller('auth')
 export class AuthController {
+  private readonly controlledErrors: ErrorHandlers<ControlledErrorKeys> = {
+    [UserErrors.USER_ALREADY_EXISTS]: (e: Error) => {
+      throw new ConflictException(e.message);
+    },
+  };
+
   constructor(private readonly authService: AuthService) {}
 
+  @Post('sign-up')
   async signUp(@Body() payload: CreateUserDto) {
-    return this.authService.signUp(payload);
+    try {
+      return await this.authService.signUp(payload);
+    } catch (error) {
+      this.controlledErrors[error.message]?.(error);
+
+      throw new InternalServerErrorException();
+    }
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { EncryptService } from './encrypt.service';
 import { UsersService } from '@/users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@/core/models/user';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
   providers: [AuthService, EncryptService, UsersService],
 })
