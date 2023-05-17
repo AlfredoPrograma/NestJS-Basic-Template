d14d68f753a9a910d9ddd02e9152b8b3f05b9c95
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-05-16 23:27:57 -0400

    feature: add sign up mehtod in Auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { CreateUserDto } from '@/core/models/user';
+import { Body, Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  async signUp(@Body() payload: CreateUserDto) {
+    return this.authService.signUp(payload);
+  }
 }
--- src/auth/tests/auth.controller.spec.ts
+++ src/auth/tests/auth.controller.spec.ts
@@ -1,14 +1,25 @@
+import { User } from '@/core/models/user';
+import { createMock } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
+import { randomUUID } from 'crypto';
 import { AuthController } from '../auth.controller';
 import { AuthService } from '../auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  const mockAuthService = createMock<AuthService>();
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -16,5 +27,28 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a registered user', async () => {
+    // Arrange
+    const generateUser = () => {
+      const user = new User();
+
+      user.id = randomUUID();
+      user.email = 'test@mail.com';
+      user.password = 'encryptedPassword';
+
+      return user;
+    };
+
+    const registeredUser = generateUser();
+
+    mockAuthService.signUp.mockResolvedValueOnce(registeredUser);
+
+    // Act
+    const result = await controller.signUp(registeredUser);
+
+    // Assert
+    expect(result).toEqual(registeredUser);
   });
 });
