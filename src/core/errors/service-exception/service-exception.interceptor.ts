acc1d58df5439ec8d954a962289c477a4d8ffdf6
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2023-06-04 17:25:22 -0400

    test: add cases for toHttp and defualt error throwing

--- src/core/errors/service-exception/service-exception.interceptor.ts
+++ src/core/errors/service-exception/service-exception.interceptor.ts
@@ -3,10 +3,7 @@ import { Observable, catchError } from 'rxjs';
 import { isServiceException } from './service-exception.error';
 
 export class ServiceExceptionInterceptor implements NestInterceptor {
-  intercept(
-    _: ExecutionContext,
-    next: CallHandler<Error>,
-  ): Observable<Error> | Promise<Observable<Error>> {
+  intercept(_: ExecutionContext, next: CallHandler<Error>): Observable<Error> {
     return next.handle().pipe(
       catchError((error: Error) => {
         if (isServiceException(error)) {
--- src/core/errors/service-exception/tests/service-exception.interceptor.spec.ts
+++ src/core/errors/service-exception/tests/service-exception.interceptor.spec.ts
@@ -1,7 +1,28 @@
+import { CallHandler, ExecutionContext, HttpException } from '@nestjs/common';
+import { createMock } from '@golevelup/ts-jest';
+import { lastValueFrom, throwError } from 'rxjs';
+
 import { ServiceExceptionInterceptor } from '../service-exception.interceptor';
+import { ServiceException } from '../service-exception.error';
+
+class TestServiceException extends ServiceException {
+  constructor() {
+    super();
+    this.message = 'Test exception';
+  }
+
+  toHttp() {
+    return new HttpException(this.message, 400);
+  }
+}
 
 describe('ServiceExceptionInterceptor', () => {
   let interceptor: ServiceExceptionInterceptor;
+
+  const mockExecutionContext = createMock<ExecutionContext>();
+  const mockCallHandler = createMock<CallHandler>({
+    handle: () => throwError(() => new TestServiceException()),
+  });
 
   beforeEach(() => {
     interceptor = new ServiceExceptionInterceptor();
@@ -9,5 +30,42 @@ describe('ServiceExceptionInterceptor', () => {
 
   it('should be defined', () => {
     expect(interceptor).toBeDefined();
+  });
+
+  it('should throw http exception', async () => {
+    // Arrange
+    mockCallHandler.handle.mockImplementationOnce(() =>
+      throwError(() => new TestServiceException()),
+    );
+
+    // Act
+    const errorObservable = interceptor.intercept(
+      mockExecutionContext,
+      mockCallHandler,
+    );
+
+    // Assert
+    await expect(lastValueFrom(errorObservable)).rejects.toThrowError(
+      new TestServiceException().toHttp(),
+    );
+  });
+
+  it('should throw given error as default', async () => {
+    // Arrange
+    const nonServiceExceptionError = new Error('Non service exception error');
+    mockCallHandler.handle.mockImplementationOnce(() =>
+      throwError(() => nonServiceExceptionError),
+    );
+
+    // Act
+    const errorObservable = interceptor.intercept(
+      mockExecutionContext,
+      mockCallHandler,
+    );
+
+    // Assert
+    await expect(lastValueFrom(errorObservable)).rejects.toThrowError(
+      nonServiceExceptionError,
+    );
   });
 });
